61a79dbb93d4122762b13c58ee96c00fed5c0afe
ChocoBooyy <goombaniv1@gmail.com>
2026-03-14 15:25:50 +0100

    refactor(kinetics): extract DirectionalMotorBlock, DirectionalMotorValueBox, and ShaftHalfRenderer to eliminate duplicated brake/motor code

---
+++ src/main/java/com/chocoboy/create_henry/content/blocks/kinetics/DirectionalMotorBlock.java
@@ -0,0 +1,56 @@
+package com.chocoboy.create_henry.content.blocks.kinetics;
+
+import com.simibubi.create.AllShapes;
+import com.simibubi.create.content.kinetics.base.DirectionalKineticBlock;
+import com.simibubi.create.foundation.block.IBE;
+import com.simibubi.create.foundation.blockEntity.SmartBlockEntity;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.LevelReader;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.pathfinder.PathComputationType;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.VoxelShape;
+
+public abstract class DirectionalMotorBlock<T extends SmartBlockEntity> extends DirectionalKineticBlock implements IBE<T> {
+
+    public DirectionalMotorBlock(BlockBehaviour.Properties properties) {
+        super(properties);
+    }
+
+    @Override
+    public VoxelShape getShape(BlockState state, BlockGetter worldIn, BlockPos pos, CollisionContext context) {
+        return AllShapes.MOTOR_BLOCK.get(state.getValue(FACING));
+    }
+
+    @Override
+    public BlockState getStateForPlacement(BlockPlaceContext context) {
+        Direction preferred = getPreferredFacing(context);
+        return (context.getPlayer() == null || !context.getPlayer().isShiftKeyDown()) && preferred != null
+                ? defaultBlockState().setValue(FACING, preferred)
+                : super.getStateForPlacement(context);
+    }
+
+    @Override
+    public boolean hasShaftTowards(LevelReader world, BlockPos pos, BlockState state, Direction face) {
+        return face == state.getValue(FACING);
+    }
+
+    @Override
+    public Direction.Axis getRotationAxis(BlockState state) {
+        return state.getValue(FACING).getAxis();
+    }
+
+    @Override
+    public boolean hideStressImpact() {
+        return true;
+    }
+
+    @Override
+    public boolean isPathfindable(BlockState state, BlockGetter reader, BlockPos pos, PathComputationType type) {
+        return false;
+    }
+}
--- src/main/java/com/chocoboy/create_henry/content/blocks/kinetics/industrial_brake/IndustrialBrakeBlock.java
+++ src/main/java/com/chocoboy/create_henry/content/blocks/kinetics/industrial_brake/IndustrialBrakeBlock.java
@@ -1,55 +1,22 @@
 package com.chocoboy.create_henry.content.blocks.kinetics.industrial_brake;
 
-import com.simibubi.create.AllShapes;
-import com.simibubi.create.content.kinetics.base.DirectionalKineticBlock;
-import com.simibubi.create.foundation.block.IBE;
-import net.minecraft.core.BlockPos;
-import net.minecraft.core.Direction;
-import net.minecraft.world.item.context.BlockPlaceContext;
-import net.minecraft.world.level.BlockGetter;
-import net.minecraft.world.level.LevelReader;
+import com.chocoboy.create_henry.content.blocks.kinetics.DirectionalMotorBlock;
+import com.chocoboy.create_henry.registry.HenryBlockEntityTypes;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
-import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.pathfinder.PathComputationType;
-import net.minecraft.world.phys.shapes.CollisionContext;
-import net.minecraft.world.phys.shapes.VoxelShape;
-import com.chocoboy.create_henry.registry.HenryBlockEntityTypes;
 
-public class IndustrialBrakeBlock extends DirectionalKineticBlock implements IBE<IndustrialBrakeBlockEntity> {
+public class IndustrialBrakeBlock extends DirectionalMotorBlock<IndustrialBrakeBlockEntity> {
+
     public IndustrialBrakeBlock(BlockBehaviour.Properties properties) {
         super(properties);
     }
 
-    public VoxelShape getShape(BlockState state, BlockGetter worldIn, BlockPos pos, CollisionContext context) {
-        return AllShapes.MOTOR_BLOCK.get((Direction)state.getValue(FACING));
-    }
-
-    public BlockState getStateForPlacement(BlockPlaceContext context) {
-        Direction preferred = this.getPreferredFacing(context);
-        return (context.getPlayer() == null || !context.getPlayer().isShiftKeyDown()) && preferred != null ? (BlockState)this.defaultBlockState().setValue(FACING, preferred) : super.getStateForPlacement(context);
-    }
-
-    public boolean hasShaftTowards(LevelReader world, BlockPos pos, BlockState state, Direction face) {
-        return face == state.getValue(FACING);
-    }
-
-    public Direction.Axis getRotationAxis(BlockState state) {
-        return ((Direction)state.getValue(FACING)).getAxis();
-    }
-
-    public boolean hideStressImpact() {
-        return true;
-    }
-
-    public boolean isPathfindable(BlockState state, BlockGetter reader, BlockPos pos, PathComputationType type) {
-        return false;
-    }
-
+    @Override
     public Class<IndustrialBrakeBlockEntity> getBlockEntityClass() {
         return IndustrialBrakeBlockEntity.class;
     }
 
+    @Override
     public BlockEntityType<? extends IndustrialBrakeBlockEntity> getBlockEntityType() {
         return HenryBlockEntityTypes.INDUSTRIAL_BRAKE.get();
     }
