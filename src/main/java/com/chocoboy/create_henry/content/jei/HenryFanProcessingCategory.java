7caf908bd048b8a3230cb40f9734f1dac19de955
ChocoBooyy <goombaniv1@gmail.com>
2026-06-27 23:11:46 +0200

    refactor(jei): drop the one-line fan categories and colocate the plugin

--- src/main/java/com/chocoboy/create_henry/content/jei/HenryFanProcessingCategory.java
+++ src/main/java/com/chocoboy/create_henry/content/jei/HenryFanProcessingCategory.java
@@ -141,7 +141,7 @@ public abstract class HenryFanProcessingCategory<T extends Recipe<?>> extends Cr
 
     }
 
-    public static abstract class SimpleBlock<T extends ProcessingRecipe<?>> extends MultiOutput<T> {
+    public static class SimpleBlock<T extends ProcessingRecipe<?>> extends MultiOutput<T> {
 
         private final BlockState blockState;
 
